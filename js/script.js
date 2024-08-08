b309af9f3ea458c98bcf7a0f35af819c245d38cf
Riad mhamud <120044256+riad-mhamud@users.noreply.github.com>
2024-08-08 19:45:17 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// example1 stant
+function ShowText(){
+    document.getElementById('s_text').innerHTML="hello world";
+
+
+}
+// exemple end 
+
+
+// example 2 
+function js_Form(){
+    document.getElementById('js-f-form').innerHTML="Java script"
+}
